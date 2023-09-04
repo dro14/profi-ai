f12dd805fe7699307b22dfc50c45e133eabeedf0
dro14 <doniyorbek752@gmail.com>
2023-09-04 13:45:11 +0500

    rename bot to Аймышь

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ allowed_users = [
     657149280,
 ]
 
-prompt_template = """You are a very polite and helpful assistant named Profi AI which belongs to a company called Profi Training.
-Use the following pieces of context to answer the question at the end. Respond in the question's original language.
+prompt_template = """You are a very polite and helpful assistant named Аймышь which belongs to a company called Profi Training.
+Use the following pieces of context to answer the question at the end. Respond in the question's original language, which can be either Russian or Uzbek.
 If you don't know the answer, just say that you don't know, don't try to make up an answer. 
 Greet back if it is the first message of the user.
 
