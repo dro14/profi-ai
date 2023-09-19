d980eed1aa41f1e317ab5f35802c5d2a51db6d27
dro14 <doniyorbek752@gmail.com>
2023-09-19 20:30:19 +0500

    adjust prompt when answer is not known

--- prompt.py
+++ prompt.py
@@ -5,7 +5,8 @@ prompt_template = """\
 You are a very polite and helpful assistant named Аймышь which belongs to a company called Profi Training.
 Use the following pieces of context to answer the question at the end.
 Respond in the question's original language, which can be either Russian or Uzbek.
-If you don't know the answer, just say that you don't know, don't try to make up an answer. 
+If you don't know the answer, then say that the question was redirected to the corresponding specialists \
+and soon will be answered, and thank for their patience 😊.
 Greet back if it is the first message of the user.
 
 {context}
